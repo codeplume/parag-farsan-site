fa01d477b03e62e4ecd855f2536401d48e47f72c
codeplume <rishirajghosh@hotmail.com>
2025-08-03 19:24:06 +0530

    d

--- app/page.js
+++
@@ -1,9 +0,0 @@
-'use client';
-export default function Home() {
-  return (
-    <main className="min-h-screen flex flex-col items-center justify-center text-center p-6 bg-orange-50">
-      <h1 className="text-4xl font-bold text-orange-600 mb-4">Parag Farsan</h1>
-      <p className="text-lg text-gray-700 max-w-xl">Export-Grade Indian Farsan: Gathiya, Ratlami Sev, Papdi & More</p>
-    </main>
-  );
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
-  plugins: []
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
