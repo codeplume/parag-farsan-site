54cc30f6bea82c988ae49ed68bf13e08fbdd193e
codeplume <rishirajghosh@hotmail.com>
2025-08-03 20:11:41 +0530

    s

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,3 @@
-
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-yellow-50 text-gray-800 p-10 font-sans">
-      <h1 className="text-4xl font-bold mb-4">Parag Namkeen Homepage</h1>
-      <p className="text-lg">Welcome to the authentic taste of India crafted for global buyers.</p>
-    </div>
-  );
+  return <h1>Parag Namkeen Homepage</h1>;
 }
