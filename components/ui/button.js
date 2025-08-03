8ef35a1fa253eb1f8554a7bd8b14cca0c02c68de
codeplume <rishirajghosh@hotmail.com>
2025-08-03 19:55:54 +0530

    s

--- components/ui/button.js
+++
@@ -1,7 +0,0 @@
-export function Button({ children, className, ...props }) {
-  return (
-    <button className={`px-4 py-2 rounded bg-orange-600 hover:bg-orange-700 text-white ${className}`} {...props}>
-      {children}
-    </button>
-  );
-}
